2c46db71a612a86718a18e8a4889ca84245d5ea2
LibsterK <63710621+libsterkelsall@users.noreply.github.com>
2020-06-01 23:28:38 +0800

    determining framework

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+//script.js 01 06 2020
+
+console.log("help");
+
+jumbotron {
+    padding: 2rem 4rem;
+    //add timer and score cards
+}
+
+// vars
+
+//listen button then hide id jumboContain, start timer and display first question
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Code_Quiz
 Beat the clock ....javascript quiz
+
+First day
+Trying to determine best layout 
+Finding limitations to flexibility of jumbotron.
+Use cards??
+need text book I think...
+
+? need reset css?
